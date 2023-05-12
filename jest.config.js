ea678f552f1563f5dcf75be6382ca7b6f87680e8
marciovz <marciovz@yahoo.com.br>
2023-05-12 19:01:01 -0300

    configuração do test converage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,11 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   transformIgnorePatterns: [
     "node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-linear-gradient|react-native-iphone-x-helper)/)"
-  ]
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+  ],
+  coverageReporters: ["lcov", "json"]
 };
